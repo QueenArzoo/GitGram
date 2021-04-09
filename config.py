3f712d2cd819c2be00a2be57a99e09fee0e41c8b
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-04-09 17:41:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "1733521007:AAEeUPI72WhhE1bn21BYxTalEz1Jh3c-ndc"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "GitHuBProBot"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/QueenArzoo/GitGram"
